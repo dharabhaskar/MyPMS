5eb0167c49c21bd49b37d1b204bac29165d6092c
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-05-26 00:11:32 +0530

    Launcher page added.

---
+++ uimodule/webapp/controller/Launcher.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/infocus/MyPMS/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.infocus.MyPMS.controller.Launcher", {});
+});
