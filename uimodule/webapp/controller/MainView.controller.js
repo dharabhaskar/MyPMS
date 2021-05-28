94205c443741bd3cf3f995a21ca15e856bff8925
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-05-29 00:42:02 +0530

    All Field Binding is Done.

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,46 +1,58 @@
 sap.ui.define([
 	"com/infocus/MyPMS/controller/BaseController",
-	"sap/m/MessageBox"
-], function(Controller, MessageBox) {
+	"sap/m/MessageBox",
+	"sap/ui/model/json/JSONModel",
+], function(Controller, MessageBox, JSONModel) {
 	"use strict";
 
 	return Controller.extend("com.infocus.MyPMS.controller.MainView", {
 		onInit: function() {
+
 			var _self = this;
 			var _model = _self.getView().getModel();
-			
+			_self.empDetails = {};
 
 			sap.ui.core.BusyIndicator.show();
 
-			//Check whether the loggedIn employee is Eligible for PMS.
-			//=======================================================
-			var eligibilityCheckServiceURL = "/EmpDetailsSet('HCM_ESS')";
-			_model.read(eligibilityCheckServiceURL, {
-				method: "GET",
+			//Fetch Employee data from service...
+			var employmentSetURL = "/ConcurrentEmploymentSet?$expand=ToItems";
+			_model.read(employmentSetURL, {
 				success: function(response) {
+					sap.ui.core.BusyIndicator.hide();
 					console.log(response);
+					var empId = response.results[0].Pernr;
 
-					
-					//Get the employment details set
-					//===========================================================
-					var employmentSetURL = "/ConcurrentEmploymentSet";
-					_model.read(employmentSetURL, {
-						success: function(response) {
-							sap.ui.core.BusyIndicator.hide();
+					_self.empDetails = response.results[0];
 
-							console.log(response);
-							var empId = response.results[0].Pernr;
-							console.log("Employee ID: " + empId);
-
-						},
-						error: function() {
-							console.log('Data fetch error');
-						}
+					//DOB as Text
+					var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({
+						pattern: "dd-MM-yyyy"
 					});
-					//===========================================================
+					_self.empDetails.ExDOBText = oDateFormat.format(_self.empDetails.ExDob);
+
+					//Period of Promotion
+					var proDay = parseInt(_self.empDetails.ExPromPeriodDay);
+					proDay = proDay > 0 ? (proDay + " Day" + (proDay > 1 ? "s" : "")) : "";
+					var proMonth = parseFloat(_self.empDetails.ExPromPeriodMonth, 0);
+					proMonth = proMonth > 0 ? (proMonth + " Month" + (proMonth > 1 ? "s" : "")) : "";
+					var proYear = parseFloat(_self.empDetails.ExPromPeriodYear, 0);
+					proYear = proYear > 0 ? (proYear + " Year" + (proYear > 1 ? "s" : "")) : "";
+					_self.empDetails.PeriodOfLastPromotion = proYear + " " + proMonth + " " + proDay;
+
+					//Service In Department
+					var srvDay = parseInt(_self.empDetails.ExServiceCompDay, 0)
+					srvDay = srvDay > 0 ? (srvDay + " Day" + (srvDay > 1 ? "s" : "")) : "";
+					var srvMonth = parseFloat(_self.empDetails.ExServiceCompMonth, 0);
+					srvMonth = srvMonth > 0 ? (srvMonth + " Month" + (srvMonth > 1 ? "s" : "")) : "";
+					var srvYear = parseFloat(_self.empDetails.ExServiceCompYear, 0);
+					srvYear = srvYear > 0 ? (srvYear + " Year" + (srvYear > 1 ? "s" : "")) : "";
+					_self.empDetails.ServiceInDepartment = srvYear + " " + srvMonth + " " + srvDay;
+
+					_self.getView().setModel(new JSONModel(_self.empDetails), "emp");
+
 				},
-				error: function(error) {
-					console.log(error);
+				error: function() {
+					console.log('Data fetch error');
 				}
 			});
 		}
